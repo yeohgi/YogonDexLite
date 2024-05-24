d1f6b37668feac304ce25b1dbab32ea6c8c99926
yogi <adriankufner10@gmail.com>
2024-05-24 13:04:31 -0400

    todo and bootscript tweak

--- yogondexlite-bootscript.sh
+++ yogondexlite-bootscript.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
 yum update -y
 
@@ -8,16 +7,19 @@ yum install -y httpd
 yum install -y php
 yum install -y tmux
 yum install -y git
-
 yum install python3-pip -y
+
+#pip install
 pip3 install dask
 pip3 install fuzzywuzzy
 pip3 install pandas
 pip3 install python-Levenshtein
 
+#create a new directory to import YogonDexLite files
 mkdir -p ydl
 cd ydl
 
 git clone https://github.com/yeohgi/YogonDexLite.git .
 
-tmux new-session -d -s myserver "python3 awsserver.py 58061"
+#create a new tmux session to let the server run indefinitly
+tmux new-session -d -s myserver "python3 server.py 80"
